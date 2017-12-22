0d50c8a22eb740634de4a3cfaeade12260653a7f
thehouseplant <sean.collins@outlook.com>
2017-12-22 15:55:38 -0800

    Broke Footer and Navbar into separate components

---
+++ client/src/Footer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer className="footer">
+          <div className="container">
+            <span className="text-muted">
+              <p className="text-center">
+                Copyright 2017 <a href="https://www.reddit.com/r/CrewsCrew/">CrewsCrew</a>
+              </p>
+            </span>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
+      
+export default Footer;
